4df91e5aa971517789de362ca3673b9cc3e18bc3
Rodrigo Gunisalvo <gunisalvo@gmail.com>
2016-08-26 21:54:04 -0300

    Update Documentation

--- toolkit/digitalocean_cli.py
+++ toolkit/digitalocean_cli.py
@@ -10,45 +10,43 @@ import json
 DO_TOKEN = os.environ["DO_TOKEN"]
 
 available_actions = {
+    "status": {
+        "method": "_GET",
+        "type": None,
+        "extra-args": [],
+        "uri-postfix": ""},
     "start": { 
-        "method" : "_POST", 
-        "type" : "power_on", 
-        "extra-args" : [], 
-        "uri-postfix" : "/actions" },
+        "method": "_POST",
+        "type": "power_on",
+        "extra-args": [],
+        "uri-postfix": "/actions"},
     "stop": { 
-        "method" : "_POST", 
-        "type" : "power_off", 
-        "extra-args" : [], 
-        "uri-postfix" : 
-        "/actions" },
+        "method": "_POST",
+        "type": "power_off",
+        "extra-args": [],
+        "uri-postfix":
+        "/actions"},
     "snapshot": { 
-        "method" : "_POST",
-        "type" : "snapshot", 
-        "extra-args" : ["name"],
-        "uri-postfix" : "/actions" },
+        "method": "_POST",
+        "type": "snapshot",
+        "extra-args": ["name"],
+        "uri-postfix": "/actions"},
     "delete": { 
-        "method" : "_DELETE",
-        "type" : None,
-        "extra-args" : [], 
-        "uri-postfix" : "" }
+        "method": "_DELETE",
+        "type": None,
+        "extra-args": [],
+        "uri-postfix": ""}
 }
 
 
 def get_command_line_parser():
     parser = OptionParser()
-    parser.add_option('-n','--node', \
-        dest='node', \
-        help='the path of a digitalocean node creation report')
-    parser.add_option('-a','--action', \
-        dest='action', \
-        help='the action to be performed on a digitalocean node', \
-        choices=['start','stop','snapshot','delete'])
-    parser.add_option('-v','--verbose', \
-        action="store_true", \
-        dest="verbose")
-    parser.add_option('-x','--extra', \
-        dest="extra", \
-        help='JSON formatted dictionary of extra arguments for specific action')
+    parser.add_option('-n', '--node', dest='node', help='the path of a digital ocean node creation report')
+    parser.add_option('-a', '--action', dest='action', help='the action to be performed on a digital ocean node',
+                      choices=['status', 'start', 'stop', 'snapshot', 'delete'])
+    parser.add_option('-v', '--verbose', action="store_true", dest="verbose")
+    parser.add_option('-x','--extra', dest="extra",
+                      help='JSON formatted dictionary of extra arguments for specific action')
 
     return parser
 
@@ -66,6 +64,20 @@ def load_node_info(info_path):
     return configuration
 
 
+def _GET(action, url, extra_args, volumes, debug=False):
+    full_url = "{0}{1}".format(url, action['uri-postfix'])
+    headers = {
+        "Authorization": "Bearer {0}".format(DO_TOKEN),
+    }
+
+    resp = requests.get(full_url, headers=headers)
+    if debug:
+        data = dump.dump_all(resp)
+        print(data.decode('utf-8'))
+
+    return json.dumps(resp.json()['droplet'], indent=2)
+
+
 def _POST(action, url, extra_args, volumes, debug=False):
     payload = { "type" : action['type'] }
     
@@ -77,8 +89,8 @@ def _POST(action, url, extra_args, volumes, debug=False):
         "Authorization": "Bearer {0}".format(DO_TOKEN),
         "Content-Type": "application/json"
     }
-    full_url = "{0}{1}".format(url,action['uri-postfix'])
-    print("{0} -> {1}".format(action['type'],full_url))
+    full_url = "{0}{1}".format(url, action['uri-postfix'])
+    print("{0} -> {1}".format(action['type'], full_url))
     resp = requests.post(full_url, json=payload, headers=headers)
     if debug:
         data = dump.dump_all(resp)
@@ -86,11 +98,11 @@ def _POST(action, url, extra_args, volumes, debug=False):
 
 
 def _DELETE(action, url, extra_args, volumes, debug=False):
-    full_url = "{0}{1}".format(url,action['uri-postfix'])
+    full_url = "{0}{1}".format(url, action['uri-postfix'])
     __http_delete(full_url, debug)
 
     for volume in volumes:
-        full_url = "{0}{1}".format(volume,action['uri-postfix'])
+        full_url = "{0}{1}".format(volume, action['uri-postfix'])
         __http_delete(full_url, debug)
 
 
@@ -98,7 +110,7 @@ def __http_delete(full_url, debug):
     headers = {
         "Authorization": "Bearer {0}".format(DO_TOKEN),
     }
-    print("{0} -> {1}".format(action['type'],full_url))
+    print("{0} -> {1}".format(action['type'], full_url))
     resp = requests.delete(full_url, headers=headers)
     if debug:
         data = dump.dump_all(resp)
@@ -118,11 +130,8 @@ if __name__ == '__main__':
         if action['extra-args']:
             extra_args = json.loads(options.extra)
         for node in configuration['nodes']:
-            locals()[action['method']](action, \
-                node['url'], \
-                extra_args, \
-                node['volumes'], \
-                options.verbose)
+            result = locals()[action['method']](action, node['url'], extra_args, node['volumes'], options.verbose)
+            print(result)
         
         exit(0)
     except OptionError as opt_error:
